3ddd41edb0496acf230e8bd7a5e1dc9ac6733ba3
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-19 23:07:19 +0100

    Update and rename virtualbotmain.py to firebotmain.py

---
+++ firebotmain.py
@@ -0,0 +1,6 @@
+# hmm
+import os
+
+os.system(
+    "git clone https://github.com/Chrisdroid1/Fire-X /root/firebot && mkdir /root/firebot/bin/ && cd /root/firebot/ && chmod +x /usr/local/bin/* && python3 -m firebot"
+)
